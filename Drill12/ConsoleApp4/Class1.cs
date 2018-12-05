5641fe86164f47ef1fb01a53d5c082998993d4e7
rohitudas <43554634+rohitudas@users.noreply.github.com>
2018-12-05 01:34:50 -0800

    Drill 12 commit

---
+++ Drill12/ConsoleApp4/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Drill12;
+
+namespace Drill12
+{
+    class Class1
+    {
+        static void Main()
+        {
+            string numberS;
+            int number;
+
+            QuickMaths GetNumber = new QuickMaths();
+            Console.Write("Enter Integer to do some quick maffs:");
+            numberS = Console.ReadLine();
+            number = Convert.ToInt16(numberS);
+            number = GetNumber.Addition(number);
+            number = GetNumber.Mutlipy(number);
+            number = GetNumber.Subtraction(number);
+
+            Console.WriteLine(number);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Drill12/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill12
+{ 
+    class QuickMaths
+    {
+        public int Mutlipy(int Multi)
+        {
+            Multi = Multi * 10;
+
+            return Multi;
+        }
+        public int Addition(int Add)
+        {
+            Add = Add + 100;
+
+            return Add;
+        }
+        public int Subtraction(int Minus)
+        {
+            Minus = Minus - 100;
+
+            return Minus;
+        }
+    }
+}
