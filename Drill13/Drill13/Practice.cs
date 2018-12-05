d45a07a4d48ce84a5501b9e1c9b7267a5162cbc7
rohitudas <43554634+rohitudas@users.noreply.github.com>
2018-12-05 02:07:16 -0800

    Drill 13 commit

---
+++ Drill13/Drill13/Practice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill13
+{
+    class Practice
+    {
+        public int Method1(int num1,int num2)
+        {
+            num1 = num1 * 2;
+            Console.WriteLine(num2);
+            return num1;
+        }
+    }
+}
---
+++ Drill13/Drill13/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practice doit = new Practice();
+            int number = doit.Method1(1, 2);
+            Console.ReadLine();
+
+        }
+    }
+}
