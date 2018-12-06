3005df6cafb294bbd22f35b1e524dc42e9c045f4
rohitudas <43554634+rohitudas@users.noreply.github.com>
2018-12-05 17:51:02 -0800

    Drill13b right version

---
+++ Drill13b/Drill13b/Practice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill13b
+{
+    class Practice
+    {
+      
+        public void Method1(int num1, int num2)
+        {
+            num1 = num1 * 2;
+            Console.WriteLine(num2);
+        }
+       
+    }
+}
---
+++ Drill13b/Drill13b/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill13b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practice doit = new Practice();
+            doit.Method1(3, 5);
+            Console.ReadLine();
+        }
+    }
+}
