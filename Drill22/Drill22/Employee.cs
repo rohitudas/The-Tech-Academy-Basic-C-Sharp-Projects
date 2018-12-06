f6142ef395444089c5469a9d4bf150b3e30d8257
rohitudas <43554634+rohitudas@users.noreply.github.com>
2018-12-06 00:27:52 -0800

    Drill 22(23) Commit

---
+++ Drill22/Drill22/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill22
+{
+    public class Employee
+    {
+        public List<int> ID { get; set; }
+        public List<string> FirstName { get; set; }
+        public List<string> LastName { get; set; }
+
+    }
+}
---
+++ Drill22/Drill22/Program.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee Employees = new Employee();
+            Employees.FirstName = new List<string>();
+            Employees.LastName = new List<string>();
+            Employees.ID = new List<int>();
+
+            Employees.ID.Add(0);
+            Employees.FirstName.Add("Bill");
+            Employees.LastName.Add("man");
+
+            Employees.ID.Add(1);
+            Employees.FirstName.Add("bob");
+            Employees.LastName.Add("manner");
+
+            Employees.ID.Add(2);
+            Employees.FirstName.Add("Betsy");
+            Employees.LastName.Add("mans");
+
+            Employees.ID.Add(3);
+            Employees.FirstName.Add("Bruh");
+            Employees.LastName.Add("man");
+
+            Employees.ID.Add(4);
+            Employees.FirstName.Add("Bro");
+            Employees.LastName.Add("tho");
+
+            Employees.ID.Add(5);
+            Employees.FirstName.Add("Joe");
+            Employees.LastName.Add("Shmo");
+
+            Employees.ID.Add(6);
+            Employees.FirstName.Add("Joe");
+            Employees.LastName.Add("Flo");
+
+            Employees.ID.Add(7);
+            Employees.FirstName.Add("Joe");
+            Employees.LastName.Add("ey");
+
+            Employees.ID.Add(8);
+            Employees.FirstName.Add("JOEY");
+            Employees.LastName.Add("WHEELER");
+
+            Employees.ID.Add(9);
+            Employees.FirstName.Add("Help");
+            Employees.LastName.Add("Me");
+
+            List<string> JoeList = new List<string>();
+            int indexCount = 0;
+
+            foreach(string employee in Employees.FirstName)
+            {
+
+                if (employee == "Joe")
+                {
+                    JoeList.Add(employee + " " + Employees.LastName[indexCount]);
+                }
+                indexCount++;
+            }
+            foreach(string ex in JoeList)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
