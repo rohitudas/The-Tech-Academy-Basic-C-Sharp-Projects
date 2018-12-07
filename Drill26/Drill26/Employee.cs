d52d6c524114dddbda9bfeae8c47088b521f51cc
rohitudas <43554634+rohitudas@users.noreply.github.com>
2018-12-07 00:25:19 -0800

    Drill 26 Commit

---
+++ Drill26/Drill26/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill26
+{
+    public class Employee
+    {
+        public Employee(string name) : this(name, 0)
+        {
+
+        }
+        public Employee(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+        
+        
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+    }
+}
